e37bca526e51f1692be6fd9c901a76e16379526f
Bradley Allen <allen.bradley@ymail.com>
2019-02-25 12:55:53 -0600

    test getting started after changing .git/config on github to push to two repositories

--- testfiles/sample.sh
+++ testfiles/sample.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# 	testfiles/sample.sh  10.10.10.498  2019-02-25T12:55:53.346468-06:00 (CST)  https://github.com/cptx86/markit.git  uadmin  one-rpi3b.cptx86.com 3.206-3-g31765fb  
+# 	   test getting started after changing .git/config on github to push to two repositories 
 # 	testfiles/sample.sh  3.197.368  2019-02-08T20:33:53.600661-06:00 (CST)  https://github.com/BradleyA/markit  uadmin  six-rpi3b.cptx86.com 3.196-1-g46f4c0d  
 # 	   test markit for sample.sh 
 #
+#	test Getting Started from github fork
 #	./sample.sh
 ###
 echo "Hello World"
